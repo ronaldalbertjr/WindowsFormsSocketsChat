cab3b6edc93bd0d0674b940ccd19f55becc1cc27
ronaldalbertjr <ronaldalbert1609@gmail.com>
2016-11-08 14:39:10 -0200

    FirstCommitOfTheOnlineChat
    Made a chat for two people communicate

---
+++ Server/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textMessages = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textMyPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textMyIp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textFriendPort = new System.Windows.Forms.TextBox();
+            this.textFriendIp = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.listMessages = new System.Windows.Forms.ListBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textMessages
+            // 
+            this.textMessages.Location = new System.Drawing.Point(35, 315);
+            this.textMessages.Name = "textMessages";
+            this.textMessages.Size = new System.Drawing.Size(514, 20);
+            this.textMessages.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textMyPort);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textMyIp);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(35, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Me";
+            // 
+            // textMyPort
+            // 
+            this.textMyPort.Location = new System.Drawing.Point(55, 61);
+            this.textMyPort.Name = "textMyPort";
+            this.textMyPort.Size = new System.Drawing.Size(100, 20);
+            this.textMyPort.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Port";
+            // 
+            // textMyIp
+            // 
+            this.textMyIp.Location = new System.Drawing.Point(55, 32);
+            this.textMyIp.Name = "textMyIp";
+            this.textMyIp.Size = new System.Drawing.Size(100, 20);
+            this.textMyIp.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textFriendPort);
+            this.groupBox2.Controls.Add(this.textFriendIp);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(280, 21);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Friend";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Port";
+            // 
+            // textFriendPort
+            // 
+            this.textFriendPort.Location = new System.Drawing.Point(46, 61);
+            this.textFriendPort.Name = "textFriendPort";
+            this.textFriendPort.Size = new System.Drawing.Size(100, 20);
+            this.textFriendPort.TabIndex = 7;
+            // 
+            // textFriendIp
+            // 
+            this.textFriendIp.Location = new System.Drawing.Point(46, 35);
+            this.textFriendIp.Name = "textFriendIp";
+            this.textFriendIp.Size = new System.Drawing.Size(100, 20);
+            this.textFriendIp.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "IP";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(530, 44);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(91, 43);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // listMessages
+            // 
+            this.listMessages.FormattingEnabled = true;
+            this.listMessages.Location = new System.Drawing.Point(35, 127);
+            this.listMessages.Name = "listMessages";
+            this.listMessages.Size = new System.Drawing.Size(599, 173);
+            this.listMessages.TabIndex = 4;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(559, 315);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 360);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.listMessages);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textMessages);
+            this.Name = "Form1";
+            this.Text = "ChatApps";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textMessages;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textMyPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textMyIp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textFriendPort;
+        private System.Windows.Forms.TextBox textFriendIp;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ListBox listMessages;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ Server/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        Socket sck;
+        EndPoint epLocal, epRemote;
+        byte[] buffer;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Preparando o sockets
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            sck.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+
+            //Pegar Ip do usuario
+            textMyIp.Text = GetLocalIp();
+            textFriendIp.Text = GetLocalIp();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            epLocal = new IPEndPoint(IPAddress.Parse(textMyIp.Text), Convert.ToInt32(textMyPort.Text));
+            sck.Bind(epLocal);
+
+            epRemote = new IPEndPoint(IPAddress.Parse(textFriendIp.Text), Convert.ToInt32(textFriendPort.Text));
+            sck.Connect(epRemote);
+
+            buffer = new byte[1500];
+            sck.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote, new AsyncCallback(MessageCallBack), buffer);
+        }
+
+        string GetLocalIp()
+        {
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach(IPAddress ip in host.AddressList)
+            {
+                if(ip.AddressFamily.Equals(AddressFamily.InterNetwork))
+                {
+                    return ip.ToString();
+                }
+            }
+            return "";
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            ASCIIEncoding aEncondig = new ASCIIEncoding();
+            byte[] sendingMessages = new byte[1500];
+            sendingMessages = aEncondig.GetBytes(textMessages.Text);
+            sck.Send(sendingMessages);
+            listMessages.Items.Add("Me: " + textMessages.Text);
+            textMessages.Text = "";
+        }
+
+        void MessageCallBack(IAsyncResult aResult)
+        {
+            try
+            {
+                byte[] receivedData = new byte[1500];
+                receivedData = (byte[])aResult.AsyncState;
+                ASCIIEncoding aEncondig = new ASCIIEncoding();
+                string receivedMessages = aEncondig.GetString(receivedData);
+
+                listMessages.Items.Add("Friend: " + receivedMessages);
+
+                buffer = new byte[1500];
+                sck.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote, new AsyncCallback(MessageCallBack), buffer);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
